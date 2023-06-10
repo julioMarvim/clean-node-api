9620eec542b7c97225f3fa71a3fb9e124bddb7d8
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-10 12:37:32 -0300

    test: ensure SignUpController returns 400 if no name is provided

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 export default {
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  preset: 'ts-jest',
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	coverageProvider: 'v8',
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	preset: 'ts-jest',
 };
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,17 @@
+import { SignUpController } from './signup';
+
 describe('SignUp Controller', () => {
 	test('Shoud return 400 if no name is provideed', () => {
-		const sut = '';
-		console.log(sut);
-		const test = { ok: 'tete' };
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				email: 'any_email@mail.com',
+				password: 'any_pawwsord',
+				passwordConfirmation: 'any_password'
+			}
+		};
+		const httpResponse = sut.handle(httpRequest);
+
+		expect(httpResponse.statusCode).toBe(400);
 	});
 });

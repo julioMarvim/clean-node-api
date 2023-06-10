02b2ba74faeb02a0b2fed77483d70a8c5d6bf7a8
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-10 12:54:08 -0300

    test: SignUpController retorna erro se parametro email nao é informado

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SignUp Controller', () => {
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new Error('Missig param: name'));
 	});
+
+	test('Shoud return 400 if no email is provideed', () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				name: 'any_name',
+				password: 'any_pawwsord',
+				passwordConfirmation: 'any_password'
+			}
+		};
+		const httpResponse = sut.handle(httpRequest);
+
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(new Error('Missig param: email'));
+	});
 });
--- test.ts
+++
@@ -1,5 +0,0 @@
-function teste(): number {
-	return 1;
-}
-const response = teste();
-console.log('count: %d', response);

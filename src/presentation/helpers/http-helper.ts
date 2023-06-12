717df05bd53f948ca70228c9e1422b74f940b48a
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-12 15:00:07 -0300

    fix: corrige erros eslint

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,6 @@
-import { HttpResponse } from '../protocols/http';
+import { type HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
-	statusCode: 400,
-	body: error
-});
+  statusCode: 400,
+  body: error
+})

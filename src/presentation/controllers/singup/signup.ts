ad71f4d27f384b9158d65806141b1754ef11283c
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-13 10:38:34 -0300

    refactor: move retorno de sucesso para um metodo de helper

--- src/presentation/controllers/singup/signup.ts
+++ src/presentation/controllers/singup/signup.ts
@@ -1,6 +1,6 @@
 import { type HttpRequest, type HttpResponse, type EmailValidator, type Controller, type AddAccount } from './signup-protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, ok, serverError } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -34,10 +34,7 @@ export class SignUpController implements Controller {
 
       const account = this.addAccount.add({ name, email, password })
 
-      return {
-        statusCode: 200,
-        body: account
-      }
+      return ok(account)
     } catch (error) {
       return serverError()
     }

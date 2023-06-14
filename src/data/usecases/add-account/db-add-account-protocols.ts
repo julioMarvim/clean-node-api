ab080e715ba5ae9971f6eda960cb0bdc90c9e377
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-14 20:54:54 -0300

    refactor: move repository protocols para pasta data

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,4 +1,4 @@
-export * from './protocols/encrypter'
-export * from './protocols/add-account-repository'
+export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repository'
 export * from '../../../domain/models/account'
 export * from '../../../domain/usecases/add-account'

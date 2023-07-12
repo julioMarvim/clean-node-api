9ef06c1053760c0df915ecb71c9ed2597fa9a17d
julioMarvim <juliomarvim@yahoo.com.br>
2023-07-12 16:49:01 -0300

    test: garante que LogControllerDecorator retorne o mesmo resultado que o controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -45,4 +45,26 @@ describe('Log Controller Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('Should return de same result of the controller', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+    expect(httpResponse).toEqual({
+      statusCode: 200,
+      body: {
+        name: 'Julio'
+      }
+    })
+  })
 })

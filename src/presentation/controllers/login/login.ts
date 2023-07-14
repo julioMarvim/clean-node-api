80cc953dc8f085937e4b5ec44b7361a4b5383f6b
julioMarvim <juliomarvim@yahoo.com.br>
2023-07-14 13:43:30 -0300

    feat: garante que LoginController 400 se o password nao for informado

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -4,6 +4,12 @@ import { type HttpRequest, type Controller, type HttpResponse } from '../../prot
 
 export class LoginController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
+    if (!httpRequest.body.email) {
+      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('email'))) })
+    }
+
+    if (!httpRequest.body.password) {
+      return await new Promise(resolve => { resolve(badRequest(new MissingParamError('password'))) })
+    }
   }
 }

4ecfadfba0d8408d88015e711047fa80e544d6eb
julioMarvim <juliomarvim@yahoo.com.br>
2023-07-16 17:14:55 -0300

    refactor: move os fields obrigatorios para um ValidationComposity

---
+++ src/main/factories/signup-validation.ts
@@ -0,0 +1,12 @@
+import { ValidationComposite } from '../../presentation/helpers/validatiors/required-field-validation'
+import { type Validation } from '../../presentation/helpers/validatiors/validation'
+import { RequiredFieldValidation } from '../../presentation/helpers/validatiors/validation-composite'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  return new ValidationComposite(validations)
+}

5658652c28efa6014205bb2f77a29a099a702b6f
julioMarvim <juliomarvim@yahoo.com.br>
2023-07-17 11:58:42 -0300

    fix: corrige semantica de variaveis do teste

--- src/presentation/helpers/validatiors/required-field-validation.spec.ts
+++ src/presentation/helpers/validatiors/required-field-validation.spec.ts
@@ -7,13 +7,13 @@ const makeSut = (): RequiredFieldValidation => {
 describe('RequiredField Validation', () => {
   test('Should return a MissingParamError if validation fails', () => {
     const sut = makeSut()
-    const error = sut.validate({ invalidField: 'any_invalid_field' })
+    const error = sut.validate({ wrongField: 'any_value' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
   test('Should not return if validation succeeds', () => {
     const sut = makeSut()
-    const error = sut.validate({ field: 'any_field' })
+    const error = sut.validate({ field: 'any_value' })
     expect(error).toBeFalsy()
   })
 })

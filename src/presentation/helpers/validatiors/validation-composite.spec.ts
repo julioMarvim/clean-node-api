f4f648cbf4c3046443cb3617d4bfc79adb61dcbd
julioMarvim <juliomarvim@yahoo.com.br>
2023-07-17 13:13:38 -0300

    test: garante que ValidationComposite nao retorna nada em caso de sucesso

--- src/presentation/helpers/validatiors/validation-composite.spec.ts
+++ src/presentation/helpers/validatiors/validation-composite.spec.ts
@@ -53,4 +53,11 @@ describe('Validation Composite', () => {
 
     expect(error).toEqual(new Error())
   })
+
+  test('Should not retorn if validation socceeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+
+    expect(error).toBeFalsy()
+  })
 })

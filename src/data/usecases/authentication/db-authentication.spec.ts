963f2165906b08f8632cf3df02f29ce3ccdd943b
julioMarvim <juliomarvim@yahoo.com.br>
2023-07-26 17:07:08 -0300

    test: garante que DbAuthentication quebre se HashComparer quebrar

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -82,4 +82,13 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
+
+  test('Should throw if hashComparerStup throws', async () => {
+    const { sut, hashComparerStup } = makeSut()
+    jest.spyOn(hashComparerStup, 'compare').mockReturnValueOnce(
+      new Promise((resolve, reject) => { reject(new Error()) })
+    )
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
 })

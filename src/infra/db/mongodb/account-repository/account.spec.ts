f14168f32f0e7fb613daf0a37c47d868c2c03b2d
julioMarvim <juliomarvim@yahoo.com.br>
2023-08-02 22:16:46 -0300

    test: garante que AccountMongoRepository atualize o accessToken quando updateAccessToken funcionar com sucesso

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -61,4 +61,20 @@ describe('Account Mongo Repository', () => {
 
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    await sut.updateAccessToken(res.insertedId.toString(), 'any_token')
+    const account = await accountCollection.findOne({ _id: res.insertedId })
+
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
+  })
 })
